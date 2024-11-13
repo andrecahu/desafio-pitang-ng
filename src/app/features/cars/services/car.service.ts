3c8aa3789db444ce45dfdafe9900098ab5c9b504
André Cahú <andrecahu28@gmail.com>
2024-11-12 21:54:20 -0300

    feat/2.1: Registrar Carro no Perfil - Adicionado a inserção de carro para o usuário logado

--- src/app/features/cars/services/car.service.ts
+++ src/app/features/cars/services/car.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environment/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from '../../../models/user.model';
+import {Observable} from 'rxjs';
+import {SignInResponse} from '../../../models/sign-in-response.model';
+import {Car} from '../../../models/car.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/cars`;
+
+  constructor(private http: HttpClient) { }
+
+  createCar(car: Car): Observable<SignInResponse> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<SignInResponse>(this.apiUrl, car, {headers}).pipe();
+  }
 }
